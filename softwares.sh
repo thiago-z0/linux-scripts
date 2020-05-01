adaaa0e3636270813b7609de47be932b26610de2
Thiago Ribeiro Correia <xpmedia4k@gmail.com>
2020-04-30 23:57:57 -0300

    Update softwares.sh

--- softwares.sh
+++ softwares.sh
@@ -22,6 +22,10 @@ sudo snap install discord &&
 
 cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb && 
 
+sudo docker run --name postgres -e POSTGRES_PASSWORD=docker -p 5432:5432  -d postgres:11 &&
+
+sudo docker run --name mongo -p 27017:27017  -d mongo:4 &&
+
 sudo apt update && sudo apt dist-upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y 
 
 echo "Finalizado"
